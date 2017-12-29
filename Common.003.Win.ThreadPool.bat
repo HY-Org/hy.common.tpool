e962ff8432b899b928189660acdcdae7f48f6129
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-29 12:07:15 +0800

    1.1.1
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- Common.003.Win.ThreadPool.bat
+++ Common.003.Win.ThreadPool.bat
@@ -5,7 +5,7 @@ cd .\bin
 rd /s/q .\org\hy\common\cache\junit
 rd /s/q .\org\hy\common\thread\junit
 
-jar cvfm hy.common.tpool.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.tpool.jar MANIFEST.MF META-INF org
 
 copy hy.common.tpool.jar ..
 del /q hy.common.tpool.jar
--- Common.004.Mac.ThreadPool.sh
+++ Common.004.Mac.ThreadPool.sh
@@ -5,7 +5,7 @@ cd ./bin
 rm -R ./org/hy/common/cache/junit
 rm -R ./org/hy/common/thread/junit
 
-jar cvfm hy.common.tpool.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.tpool.jar MANIFEST.MF META-INF org
 
 cp hy.common.tpool.jar ..
 rm hy.common.tpool.jar
