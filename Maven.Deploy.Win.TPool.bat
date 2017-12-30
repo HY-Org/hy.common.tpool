e6390937206bdc2a39d18948bbfd3de4bca34c72
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-30 10:26:03 +0800

    1.1.1
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

---
+++ Maven.Deploy.Win.TPool.bat
@@ -0,0 +1,1 @@
+mvn deploy:deploy-file -Dfile=hy.common.tpool.jar -DpomFile=./src/META-INF/maven/org/hy/common/tpool/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
