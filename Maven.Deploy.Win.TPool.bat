e4616cf8099be68074698c9634d209c4d1a611b7
HY-ZhengWei <hy.zhengwei@qq.com>
2019-09-04 16:22:57 +0800

    1.2.3

--- Maven.Deploy.Win.TPool.bat
+++ Maven.Deploy.Win.TPool.bat
@@ -1,2 +1,2 @@
-start mvn deploy:deploy-file -Dfile=hy.common.tpool.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/tpool/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.common.tpool-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/tpool/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.tpool.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/tpool/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.tpool-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/tpool/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
